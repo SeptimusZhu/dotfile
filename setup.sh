354d9919ce46cb7ee3846351b11a0f703d041b3d
SeptimusZhu <septimuszhu@163.com>
2018-07-18 14:53:18 +0800

    add mac branch

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 ln -f -s `pwd`/.vimrc ~/.vimrc
-ln -f -s `pwd`/.bashrc ~/.bashrc
-ln -f -s `pwd`/.bashrc ~/.bash_profile
-ln -f -s `pwd`/.tmux.conf ~/.tmux.conf
-ln -f -s `pwd`/.tmux.conf.local ~/.tmux.conf.local
-ln -f -s `pwd`/.screenrc ~/.screenrc
+ln -f -s `pwd`/.zshrc ~/.zshrc
+#ln -f -s `pwd`/.tmux.conf ~/.tmux.conf
+#ln -f -s `pwd`/.tmux.conf.local ~/.tmux.conf.local
+#ln -f -s `pwd`/.screenrc ~/.screenrc
